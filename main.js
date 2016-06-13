1526c3233b67b527b878f0d4f03bc072eaeceff0
dmh <dmytro.hrynevych@gmail.com>
2016-06-13 17:43:02 +0300

    update

--- main.js
+++ main.js
@@ -117,6 +117,7 @@ jQuery(function($) {
 
 
 // elements
+/* global Swiper*/
 (function($) {
     'use strict';
 
@@ -125,7 +126,7 @@ jQuery(function($) {
 
         // initialize swiper when document ready
         // http://idangero.us/swiper/api/
-        $('.js__img-slider').swiper({
+        var swiper = new Swiper('.js__img-slider', {
             nextButton: '.js__img-slider__btn-next',
             prevButton: '.js__img-slider__btn-prev',
             pagination: '.js__img-slider__pagination',
@@ -135,6 +136,15 @@ jQuery(function($) {
             watchSlidesVisibility: true,
             lazyLoadingInPrevNext: true,
             speed: 600
+        });
+        // Makes it possible to skip between slider images if they have links, using the tab button
+        swiper.container.on('focus', 'a', function(e) {
+            //Index of focused slide
+            var focusIndex = $(e.target).parents('.swiper-slide').index();
+            //Reset scrollLeft set by browser on focus
+            swiper.container.scrollLeft(0);
+            //Slide to focused slide
+            swiper.slideTo(focusIndex);
         });
     });
 
