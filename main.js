bb3171ca35866cee1f44e015cb2e19291b26ec99
dmh <dmytro.hrynevych@gmail.com>
2016-07-01 19:48:17 +0300

    update

--- main.js
+++ main.js
@@ -97,6 +97,9 @@ jQuery(function($) {
         e.preventDefault();
         $(this).toggleClass('_search-close-btn');
         $mainNavigationSearchBox.toggleClass('_search-box-visible');
+        if ($mainNavigationSearchBox.hasClass('_search-box-visible')) {
+            $mainNavigationSearchBox.find('input[type="search"]').focus();
+        }
         $mainNavigationSearchBoxOverlay.toggleClass('_search-box-overlay-visible');
     });
     $mainNavigationSearchBoxOverlay.on('click', function() {
@@ -230,6 +233,50 @@ jQuery(function($) {
                 noIos: true
             });
         }
+    });
+
+})(jQuery);
+
+
+// plugins
+(function($) {
+    'use strict';
+
+    // document load event
+    $(document).ready(function() {
+
+        // initialize swiper when document ready
+        // http://idangero.us/swiper/api/
+        $('.js__news-carousel').swiper({
+            nextButton: '.js__news-carousel__btn-next',
+            prevButton: '.js__news-carousel__btn-prev',
+            pagination: '.js__news-carousel__pagination',
+            paginationClickable: true,
+            slidesPerView: 4,
+            preloadImages: false,
+            spaceBetween: 30,
+
+            // Responsive breakpoints
+            breakpoints: {
+
+                // when window width is <= 480px
+                500: {
+                    slidesPerView: 1
+                },
+                // when window width is <= 768px
+                767: {
+                    slidesPerView: 2
+                },
+                // when window width is <= 992px
+                991: {
+                    slidesPerView: 3
+                },
+                // when window width is <= 1199px
+                1199: {
+                    slidesPerView: 4
+                }
+            }
+        });
     });
 
 })(jQuery);
